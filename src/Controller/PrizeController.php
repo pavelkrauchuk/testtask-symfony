27e39c12e8f85058701cfd2dbdc4b71c5b90705b
Павел <pavel.krauchuk@gmail.com>
2022-05-08 16:55:41 +0200

    Добавление логики получения приза

---
+++ src/Controller/PrizeController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\AvailableThing;
+use App\Entity\Bonus;
+use App\Entity\Money;
+use App\Entity\Parameters;
+use App\Entity\Prize;
+use App\Entity\Thing;
+use App\RandomPrizeGenerator;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PrizeController extends AbstractController
+{
+    #[Route('/getprize', name: 'app_get_prize')]
+    public function getPrize(EntityManagerInterface $entityManager): Response
+    {
+        $availableTypes = Prize::getAvailableTypes($entityManager);
+        $prize = RandomPrizeGenerator::generate($availableTypes);
+        $currentUser = $this->getUser();
+
+        if ($prize instanceof Money) {
+            $moneyValue = RandomPrizeGenerator::getRandomMoneyValue($entityManager);
+            $prize->setAmount($moneyValue)
+                ->setIsTransferred(false)
+                ->setIsConverted(false)
+                ->setUser($currentUser);
+
+            $entityManager->persist($prize);
+
+            $availableMoney = $entityManager->getRepository(Parameters::class)->findOneBy(array(
+                'paramName' => 'available_money'
+            ));
+
+            $availableMoney->setValue($availableMoney->getValue() - $moneyValue);
+            $entityManager->persist($availableMoney);
+        }
+
+        if ($prize instanceof Bonus) {
+            $bonusValue = RandomPrizeGenerator::getRandomBonusValue($entityManager);
+            $prize->setAmount($bonusValue)
+                ->setIsAdmissed(false)
+                ->setUser($currentUser);
+
+            $entityManager->persist($prize);
+        }
+
+        if ($prize instanceof Thing) {
+            $thing = RandomPrizeGenerator::getRandomAvailableThing($entityManager);
+            $prize->setName($thing->getName())
+                ->setIsShipped(false)
+                ->setUser($currentUser);
+
+            $entityManager->persist($prize);
+            $entityManager->remove($thing);
+
+        }
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_user_profile');
+    }
+}
--- src/RandomPrizeGenerator.php
+++ src/RandomPrizeGenerator.php
@@ -2,7 +2,12 @@
 
 namespace App;
 
+use App\Entity\AvailableThing;
+use App\Entity\Parameters;
 use App\Entity\Prize;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\Parameter;
 
 class RandomPrizeGenerator
 {
@@ -13,5 +18,33 @@ class RandomPrizeGenerator
         $class = '\\App\\Entity\\' . ucfirst($type);
 
         return new $class();
+    }
+
+    public static function getRandomMoneyValue(EntityManagerInterface $entityManager) : int
+    {
+        $maxMoney = $entityManager->getRepository(Parameters::class)->findOneBy(array(
+            'paramName' => 'max_money_for_prize'
+        ));
+
+        $availableMoney = $entityManager->getRepository(Parameters::class)->findOneBy(array(
+            'paramName' => 'available_money'
+        ));
+
+        $randomMoney = random_int(1, $maxMoney->getValue());
+        return min($randomMoney, $availableMoney->getValue());
+    }
+
+    public static function getRandomBonusValue(EntityManagerInterface $entityManager) : int
+    {
+        $maxBonus = $entityManager->getRepository(Parameters::class)->findOneBy(array(
+            'paramName' => 'max_bonus_for_prize'
+        ));
+
+        return random_int(1, $maxBonus->getValue());
+    }
+
+    public static function getRandomAvailableThing(EntityManagerInterface $entityManager) : AvailableThing
+    {
+        return $entityManager->getRepository(AvailableThing::class)->getRandomThing();
     }
 }
