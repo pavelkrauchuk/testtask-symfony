1250524f3c9a423f08a77575f80e9d5226e01015
Павел <pavel.krauchuk@gmail.com>
2022-05-08 22:22:43 +0200

    Создание класса BonusController

---
+++ src/Controller/BonusController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Bonus;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BonusController extends AbstractController
+{
+    #[Route('/addToAccount/{id}', name: 'app_bonus_to_account')]
+    public function addToAccount(Request $request, EntityManagerInterface $entityManager, string $id): Response
+    {
+        $submittedToken = $request->request->get('token');
+
+        if ($this->isCsrfTokenValid('add-to-account', $submittedToken)) {
+            $bonus = $entityManager->getRepository(Bonus::class)->findOneBy(array('id' => $id));
+            $user = $this->getUser();
+
+            if ($bonus?->getUser() == $user && $bonus->getIsAdmissed() == false) {
+                $user->setBonusCount($user->getBonusCount() + $bonus->getAmount());
+                $entityManager->persist($user);
+                $bonus->setIsAdmissed(true);
+                $entityManager->persist($bonus);
+
+                $entityManager->flush();
+            }
+        }
+
+        return $this->redirectToRoute('app_user_profile');
+    }
+
+    #[Route('/rejectBonus/{id}', name: 'app_bonus_reject')]
+    public function rejectBonus(Request $request, EntityManagerInterface $entityManager, string $id) : Response
+    {
+        $submittedToken = $request->request->get('token');
+
+        if ($this->isCsrfTokenValid('reject-bonus', $submittedToken)) {
+            $bonus = $entityManager->getRepository(Bonus::class)->findOneBy(array('id' => $id));
+            $user = $this->getUser();
+
+            if ($bonus?->getUser() == $user && $bonus->getIsAdmissed() == false) {
+                $entityManager->remove($bonus);
+                $entityManager->flush();
+            }
+        }
+
+        return $this->redirectToRoute('app_user_profile');
+    }
+}
