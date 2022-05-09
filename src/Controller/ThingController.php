5cb878d6252d8ffe6aba20ea6c6c1f30072703be
Павел <pavel.krauchuk@gmail.com>
2022-05-09 20:55:58 +0200

    Создание класса ThingController

---
+++ src/Controller/ThingController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\AvailableThing;
+use App\Entity\Thing;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ThingController extends AbstractController
+{
+    #[Route('/shipThing/{id}', name: 'app_thing_ship_to_user')]
+    public function shipThing(Request $request, EntityManagerInterface $entityManager, string $id): Response
+    {
+        $submittedToken = $request->request->get('token');
+
+        if ($this->isCsrfTokenValid('ship-thing-to-user', $submittedToken)) {
+            $thing = $entityManager->getRepository(Thing::class)->findOneBy(array('id' => $id));
+            $user = $this->getUser();
+
+            if ($thing->getUser() == $user && $thing->getIsShipped() == false) {
+                $thing->setIsShipped(true);
+
+                $entityManager->persist($thing);
+                $entityManager->flush();
+
+                return $this->render('thing/shipped.html.twig', [
+                    'prize' => $thing,
+                ]);
+            }
+        }
+
+        return $this->redirectToRoute('app_user_profile');
+    }
+
+    #[Route('/rejectThing/{id}', name: 'app_thing_reject')]
+    public function rejectThing(Request $request, EntityManagerInterface $entityManager, string $id): Response
+    {
+        $submittedToken = $request->request->get('token');
+
+        if ($this->isCsrfTokenValid('reject-thing', $submittedToken)) {
+            $thing = $entityManager->getRepository(Thing::class)->findOneBy(array('id' => $id));
+            $user = $this->getUser();
+
+            if ($thing->getUser() == $user && $thing->getIsShipped() == false) {
+                $availableThing = new AvailableThing($thing);
+
+                $entityManager->persist($availableThing);
+                $entityManager->remove($thing);
+                $entityManager->flush();
+            }
+        }
+
+        return $this->redirectToRoute('app_user_profile');
+    }
+}
